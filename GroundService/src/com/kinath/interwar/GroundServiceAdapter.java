6a42e3f59a2696bb945de8ddfa63636e9a610356
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-03-16 22:58:08 +0530

    updat adapter

--- GroundService/src/com/kinath/interwar/GroundServiceAdapter.java
+++ GroundService/src/com/kinath/interwar/GroundServiceAdapter.java
@@ -1,8 +1,11 @@
 package com.kinath.interwar;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Kinath on 3/15/2017.
  */
+@Configuration
 public class GroundServiceAdapter implements IGroundService
 {
     public String getMessage()
