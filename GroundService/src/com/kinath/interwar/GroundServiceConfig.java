26a8740b91c71fb58516bd6fcc596892f06f8df1
Kinath Rupasinghe <kinath.ru@gmail.com>
2017-03-16 09:59:21 +0530

    add application context

--- GroundService/src/com/kinath/interwar/GroundServiceConfig.java
+++ GroundService/src/com/kinath/interwar/GroundServiceConfig.java
@@ -1,12 +1,24 @@
 package com.kinath.interwar;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by Kinath on 3/15/2017.
  */
+@Configuration
 public class GroundServiceConfig
 {
+    private static boolean initialized;
+
     public GroundServiceConfig()
     {
+        if( initialized )
+        {
+            return;
+        }
+
+        initialized = true;
+
         if( ServiceHandler.getiGroundService() == null )
         {
             ServiceHandler.setiGroundService( new GroundServiceAdapter() );
